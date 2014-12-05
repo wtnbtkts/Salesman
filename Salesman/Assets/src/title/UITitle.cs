45bae462ab2c5b9790325dad692d8f5788184dd3
yoshimotoseiya <yoshimoto@yoshimotoseiya-no-MacBook-Air.local>
2014-12-06 01:38:10 +0900

    first commit

---
+++ Salesman/Assets/src/title/UITitle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ChangeNextScene(int buttonId)
+	{
+		switch(buttonId)
+		{
+		case 0:
+			Application.LoadLevel("quiz");
+			break;
+		default:
+			break;
+		}
+	}
+}
