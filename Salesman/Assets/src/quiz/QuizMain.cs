b376ea46591a62d96280805a7fe241feec7eedeb
yoshimoto <seiya.yoshimoto@gree.net>
2014-12-06 16:04:41 +0900

    scene

--- Salesman/Assets/src/quiz/QuizMain.cs
+++ Salesman/Assets/src/quiz/QuizMain.cs
@@ -14,21 +14,28 @@ public class QuizMain : MonoBehaviour {
 		Ray ray;
 		RaycastHit hit;
 		float distance = 1000.0f; //光線を伸ばす距離
+
 		if (Input.GetMouseButtonDown(0)) {
 			mScreen.SetKen(Ken.Chiba);
 			//メインカメラのスクリーン上のポイントを光線に変換
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			
-			//光線がhitしているオブジェクトがあるかチェック
-			//もし該当のオブジェクトがあればhitに格納される
-			if (Physics.Raycast(ray, out hit, distance))
-			{
-				Debug.Log ("hit.collider.gameObject is " + hit.collider.gameObject.name);
-				//該当オブジェクトの判別にtagを利用。ここはやりたいことに応じて適宜変更する。
-				if (hit.collider.gameObject.tag == "Touchable")
-				{
-				}
+			Vector3    aTapPoint   = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D aCollider2d = Physics2D.OverlapPoint(aTapPoint);
+			
+			if (aCollider2d) {
+				GameObject obj = aCollider2d.transform.gameObject;
 			}
 		}
+	}
+	void OnPressButton(string button) {
+		if (obj.name == "chiba") {
+			OnPressButton(Ken.Chiba);
+		} else if (obj.name == "Ibaragi") {
+			OnPressButton(Ken.Ibaragi);
+		}
+	}
+	void OnPressButton(Ken ken) {
+		mScreen.SetKen(ken);
 	}
 }
