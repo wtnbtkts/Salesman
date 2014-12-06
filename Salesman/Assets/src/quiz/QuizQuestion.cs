3f55401dc8ac8fe81a489461414d17bec6b69165
yoshimoto <seiya.yoshimoto@gree.net>
2014-12-06 14:58:25 +0900

    add quiz

--- Salesman/Assets/src/quiz/QuizQuestion.cs
+++ Salesman/Assets/src/quiz/QuizQuestion.cs
@@ -3,6 +3,41 @@ using System.Collections;
 
 public class QuizQuestion : MonoBehaviour {
 	public static string get(Ken ken, int count) {
-		return "私だった";
+		switch(ken) {
+		case Ken.Chiba:
+			return getChiba(count);
+		case Ken.Tokyo:
+			return getTokyo(count);
+		default:
+			return getChiba(count);
+		}
+	}
+	public static string getChiba(int count) {
+		switch(count) {
+		case 0:
+			return "私だった";
+		case 1:
+			return "私1";
+		case 2:
+			return "私2";
+		case 3:
+			return "私3";
+		default:
+			return "私だった4";
+		}
+	}
+	public static string getTokyo(int count) {
+		switch(count) {
+		case 0:
+			return "tokyo私だった";
+		case 1:
+			return "tokyo私1";
+		case 2:
+			return "tokyo私2";
+		case 3:
+			return "tokyo私3";
+		default:
+			return "tokyo私だった4";
+		}
 	}
 }
